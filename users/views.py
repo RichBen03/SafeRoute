07282cd2dd89f33ea3f7eb194dc524241edc0e60
Rich Benjamin <richmwendwa03@gmail.com>
2025-08-30 01:55:16 +0300

    Defined the views for the Users model{admin and user roles}

--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+from .permissions import IsAdminUserRole
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    """
+    The api POST /api/users/register/
+    """
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    """
+    The Api GET/PUT /api/users/profile/
+    returns/updates the currently authenticated user's profile
+    """
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+# Admin-only views for user management
+class UserListView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsAdminUserRole]
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdminUserRole]
+    lookup_field = "pk"
