4992dd4501b26e2f3e2fed660c1ac6ab57fcd24a
Rich Benjamin <richmwendwa03@gmail.com>
2025-09-03 17:25:26 +0300

    Registered the user app models with the admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User, Token
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    # show role and location_preference in admin list
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ('Extra', {'fields': ('role', 'location_preference')}),
+    )
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'expires_at')
+    search_fields = ('user__username', 'token')
