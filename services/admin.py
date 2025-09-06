fef3398a91cc595afb72d961eb8aa295586388e7
Rich Benjamin <richmwendwa03@gmail.com>
2025-09-07 00:36:21 +0300

    Registered the service model to the admin panel, customized the admin panel with filtering, ordering,searching and displaying fuctionalities for easy access

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "address", "lat", "lng", "contact_info", "created_at")
+    list_filter = ("type", "created_at")
+    search_fields = ("name", "address", "contact_info")
+    ordering = ("-created_at",)
