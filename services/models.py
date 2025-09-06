cb737cd00d561b5f49a5c12ef4667cfee2f9d659
Rich Benjamin <richmwendwa03@gmail.com>
2025-09-07 00:33:53 +0300

    Defined the model for the respective services that will be offered

--- services/models.py
+++ services/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    SERVICE_TYPES = [
+        ("hospital", "Hospital"),
+        ("police", "Police Station"),
+        ("fire", "Fire Station"),
+        ("pharmacy", "Pharmacy"),
+        ("school", "School"),
+        ("other", "Other"),
+    ]
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=50, choices=SERVICE_TYPES)
+    address = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    contact_number = models.CharField(max_length=20, blank=True, null=True)
+    rating = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
