a2adacbff3494824c47d2e96137cd9be9141623c
Rich Benjamin <richmwendwa03@gmail.com>
2025-09-07 17:06:31 +0300

    Registered my models to the admin interface

--- services/admin.py
+++ services/admin.py
@@ -4,7 +4,8 @@ from .models import Service
 # Register your models here.
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ("name", "type", "address", "lat", "lng", "contact_info", "created_at")
+    list_display = ("id", "name", "type", "address", "lat", "lng", "contact_info", "created_at")
     list_filter = ("type", "created_at")
     search_fields = ("name", "address", "contact_info")
     ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
