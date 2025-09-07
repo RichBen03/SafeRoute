b0eff7716fa24c014c3af135ef25cbb215d18b7a
Rich Benjamin <richmwendwa03@gmail.com>
2025-09-07 17:16:05 +0300

    registered my models to the admin panel

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import SearchHistory, SearchResult
 
-# Register your models here.
+@admin.register(SearchHistory)
+class SearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "query", "lat", "lng", "radius_km", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__email", "query")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+
+@admin.register(SearchResult)
+class SearchResultAdmin(admin.ModelAdmin):
+    list_display = ("id", "search", "service", "distance_km")
+    list_filter = ("search", "service")
+    search_fields = ("service__name", "search__user__email")
+    ordering = ("-id",)
