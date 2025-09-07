76f757525af95d1048a6a3c1d245cafe8383c8ca
Rich Benjamin <richmwendwa03@gmail.com>
2025-09-07 17:23:16 +0300

    Registered the route models to the admin interface

--- routes/admin.py
+++ routes/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Route, RouteService
 
-# Register your models here.
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "origin_lat",
+        "origin_lng",
+        "dest_lat",
+        "dest_lng",
+        "distance_km",
+        "duration_min",
+        "created_at",
+    )
+    list_filter = ("created_at",)
+    search_fields = ("user__email",)
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+
+@admin.register(RouteService)
+class RouteServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "route", "service", "distance_km")
+    list_filter = ("route", "service")
+    search_fields = ("service__name", "route__user__email")
+    ordering = ("-id",)
